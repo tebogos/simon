e4bf4659ca2efd0a60ad3857eb8ac9c51dd81227
tebogos@gmail.com <tebogos@gmail.com>
2017-08-16 17:31:42 +0200

    Added web test code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+//Dell XPS 13 (9360) or Dell XPS 13 2in1 (9365)
 
 
 const getRandomInt=(min, max)=> {
@@ -19,6 +20,49 @@ const addYourList=(state,color)=>{
     return Object.assign({},state,{yourList:[...state.yourList,color]});
 };
 
+const updateDisplayAnTone=(color)=>{
+    if(color==="red"){
+        document.getElementById("red").style.backgroundColor="#ff0000";
+//        playRedTone();
+    }
+    else if(color==="blue"){
+        document.getElementById("blue").style.backgroundColor="#0000ff";
+//        playBlueTone();
+    }
+    else if(color==="yellow"){
+        document.getElementById("yellow").style.backgroundColor="#ffff00";
+//        playBlueTone();
+    }
+    else if(color==="green"){
+        document.getElementById("green").style.backgroundColor="#00ff00";
+//        playBlueTone();
+    }
+    
+};
+const displayPlay=()=>{
+  let list=store.getState().game.colorList;
+    if(list.length>0){
+        list.forEach((color)=>{
+          updateDisplayAnTone(color);
+        });
+    }
+};
+const nextPlay=()=>{
+    if(start&&win){
+    let color =randomColorGenerator();
+    store.dispatch({type:"ADD_NEXT_PLAY",color:color});
+    }
+    displayPlay();
+    
+};
+const start=()=>{
+  
+    
+    setTimeout(play,1000);
+    
+    
+};
+
 const changeMode=(state,mode)=>{
     
     return Object.assign({},state,{mode:mode});
@@ -35,28 +79,53 @@ const increamentCounter=(state)=>{
     return Object.assign({},state,{counter:(state.counter+1)});
 };
 
-const score=(state={counter:0},action)=>{
+const updateWin=(state,win)=>{
+    
+    return Object.assign({},state,{win:win});
+};
+
+const score=(state={counter:0,win:true},action)=>{
     
   switch(action.type){
       case "INCREAMENT_COUNTER":
-          return increamentCounter(state);     
+          return increamentCounter(state);  
+      case "UPDATE_WIN":
+          return updateWin(state,action.win);  
           
     default:
           return state;
                     }  
 };
 
-const game=(state={colorList:[],yourList:[],mode:"normal"},action)=>{
+const updateStart=(state,start)=>{
+    start =  typeof start ==="boolean"?start:false;
+    return Object.assign({},state,{start:start});
+};
+const updateOn=(state,on)=>{
+    on =  typeof on ==="boolean"?on:false;
+    return Object.assign({},state,{on:on});
+};
+const updateYourTurn=(state,yourTurn)=>{
+    yourTurn =  typeof yourTurn ==="boolean"?yourTurn:false;
+    return Object.assign({},state,{yourTurn:yourTurn});
+};
+const game=(state={colorList:[],yourList:[],mode:"normal",start:false,on:false,yourTurn:false},action)=>{
     
   switch(action.type){
-      case "ADD_PLAY":
+      case "ADD_NEXT_PLAY":
           return addNextPlay(state,action.color);
-      case "ADD_YOUR_LIST":
+      case "ADD_YOUR_PLAY":
           return addYourList(state,action.color);
       case "CHANGE_MODE":
           return changeMode(state,action.mode);
       case "CLEAR_GAME":
           return clearGame(state,action.mode,action.colorList);
+      case "UPDATE_START":
+          return updateStart(state,action.start);  
+      case "UPDATE_ON":
+          return updateOn(state,action.on);  
+      case "UPDATE_YOUR_TURN":
+          return updateYourTurn(state,action.yourTurn);  
       case "CLEAR_YOUR_LIST":
           return clearYourList(state,action.mode,action.yourList);
           
@@ -65,6 +134,10 @@ const game=(state={colorList:[],yourList:[],mode:"normal"},action)=>{
                     }  
 };
 
+document.getElementById("green").addEventListener("click",()=>{
+    alert("You click me!, I am red");
+});
+
 
 const { combineReducers } = Redux;
 const simonApp = combineReducers({
@@ -72,7 +145,10 @@ const simonApp = combineReducers({
 const { createStore } =  Redux;
 const store = createStore(simonApp);
 
-console.log(store);
+store.dispatch({type:"ADD_PLAY",color:"blue"});
+store.dispatch({type:"ADD_PLAY",color:"red"});
+
+console.log(store.getState());
 
 //module.exports={    
 //    addNextPlay:addNextPlay,
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,6 @@
-var expect = require('chai').expect;
-var assert = require('chai').assert;
-var simon =require('./index');
-var chai=require('chai');
-var deepFreeze=require('deep-freeze');
+var expect = chai.expect;
+var assert = chai.assert;
+//var deepFreeze=require('deep-freeze');
 
 chai.should();
 //describe('all',function(){
@@ -25,11 +23,11 @@ describe("Game Reducer",()=>{
     it("Add color to color list",()=>{
         let beforeState={colorList:[],yourList:[],mode:"normal"};
         let afterState={colorList:["red"],yourList:[],mode:"normal"};  
-        let action={type:"ADD_PLAY",color:"red"};
+        let action={type:"ADD_NEXT_PLAY",color:"red"};
         deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.game(beforeState,action)).to.deep.equals(afterState);
+        expect(game(beforeState,action)).to.deep.equals(afterState);
         
     });
     it("Change mode",()=>{
@@ -39,7 +37,7 @@ describe("Game Reducer",()=>{
         deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.game(beforeState,action)).to.deep.equals(afterState);
+        expect(game(beforeState,action)).to.deep.equals(afterState);
         
     });
     it("Clear play list" ,()=>{
@@ -49,29 +47,78 @@ describe("Game Reducer",()=>{
         deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.game(beforeState,action)).to.deep.equals(afterState);
+        expect(game(beforeState,action)).to.deep.equals(afterState);
         
     });
     it("Clear your list" ,()=>{
         let beforeState={colorList:[],yourList:[],mode:"normal"};
         let afterState={colorList:[],yourList:[],mode:"normal"};  
-        let action ={type:"CLEAR_YOUR_LIST",mode:"normal",yourList:[]};
+        let action ={type:"CLEAR_YOUR_PLAY",mode:"normal",yourList:[]};
         deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.game(beforeState,action)).to.deep.equals(afterState);
+        expect(game(beforeState,action)).to.deep.equals(afterState);
         
     });
     it("Add color to your play list",()=>{
         let beforeState={colorList:[],yourList:[],mode:"normal"};
         let afterState={colorList:[],yourList:["red"],mode:"normal"};  
-        let action={type:"ADD_YOUR_LIST",color:"red"};
+        let action={type:"ADD_YOUR_PLAY",color:"red"};
         deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.game(beforeState,action)).to.deep.equals(afterState);
-        
+        expect(game(beforeState,action)).to.deep.equals(afterState);        
     });
+    
+    it("Update start true",()=>{
+        let beforeState={colorList:[],yourList:[],mode:"normal",start:false};
+        let afterState={colorList:[],yourList:[],mode:"normal",start:true};  
+        let action={type:"UPDATE_START",start:true};
+        deepFreeze(beforeState);
+        deepFreeze(action);        
+        expect(game(beforeState,action)).to.deep.equals(afterState);        
+    });
+    it("Update start invalit action",()=>{
+        let beforeState={colorList:[],yourList:[],mode:"normal",start:false};
+        let afterState={colorList:[],yourList:[],mode:"normal",start:false};  
+        let action={type:"UPDATE_START",start:"invalid"};
+        deepFreeze(beforeState);
+        deepFreeze(action);        
+        expect(game(beforeState,action)).to.deep.equals(afterState);        
+    });
+    it("Update on true",()=>{
+        let beforeState={colorList:[],yourList:[],mode:"normal",start:false,on:false};
+        let afterState={colorList:[],yourList:[],mode:"normal",start:false,on:true};  
+        let action={type:"UPDATE_ON",on:true};
+        deepFreeze(beforeState);
+        deepFreeze(action);        
+        expect(game(beforeState,action)).to.deep.equals(afterState);        
+    });
+    it("Update on  invalit action",()=>{
+       let beforeState={colorList:[],yourList:[],mode:"normal",start:false,on:false};
+        let afterState={colorList:[],yourList:[],mode:"normal",start:false,on:false};  
+        let action={type:"UPDATE_ON",on:"invalid"};
+        deepFreeze(beforeState);
+        deepFreeze(action);        
+        expect(game(beforeState,action)).to.deep.equals(afterState);       
+    });
+    it("Update yourTurn true",()=>{
+        let beforeState={colorList:[],yourList:[],mode:"normal",start:false,on:false,yourTurn:false};
+        let afterState={colorList:[],yourList:[],mode:"normal",start:false,on:false,yourTurn:true};  
+        let action={type:"UPDATE_YOUR_TURN",yourTurn:true};
+        deepFreeze(beforeState);
+        deepFreeze(action);        
+        expect(game(beforeState,action)).to.deep.equals(afterState);        
+    });
+    it("Update on  invalit action",()=>{
+       let beforeState={colorList:[],yourList:[],mode:"normal",start:false,on:false,yourTurn:false};
+        let afterState={colorList:[],yourList:[],mode:"normal",start:false,on:false,yourTurn:false};  
+        let action={type:"UPDATE_YOUR_TURN",yourTurn:"invalid"};
+        deepFreeze(beforeState);
+        deepFreeze(action);        
+        expect(game(beforeState,action)).to.deep.equals(afterState);      
+    });
+    
     
     
     
@@ -85,7 +132,7 @@ describe("Score Reducer",()=>{
         deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.score(beforeState,action)).to.deep.equals(afterState);
+        expect(score(beforeState,action)).to.deep.equals(afterState);
         
     });
     it("Call SOMETHING_ELSE",()=>{
@@ -95,18 +142,28 @@ describe("Score Reducer",()=>{
         deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.score(beforeState,action)).to.deep.equals(afterState);
+        expect(score(beforeState,action)).to.deep.equals(afterState);
         
     });
     
     it("undefind state",()=>{
         let beforeState=undefined;
-        let afterState={counter:1};  
+        let afterState={counter:1,win:true};  
         let action={type:"INCREAMENT_COUNTER"};
 //        deepFreeze(beforeState);
         deepFreeze(action);
         
-        expect(simon.score(beforeState,action)).to.deep.equals(afterState);
+        expect(score(beforeState,action)).to.deep.equals(afterState);
+        
+    });
+    it("Update win true",()=>{
+        let beforeState={win:false};
+        let afterState={win:true};  
+        let action={type:"UPDATE_WIN",win:true};
+        deepFreeze(beforeState);
+        deepFreeze(action);
+        
+        expect(score(beforeState,action)).to.deep.equals(afterState);
         
     });
 });
